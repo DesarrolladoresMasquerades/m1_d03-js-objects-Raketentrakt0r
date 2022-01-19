a06ebd972c28bebe2c08201d8787b836457ce44e
Luis Moncada <raketentrakt0r@gmail.com>
2022-01-19 13:03:15 +0100

    02-exercise

---
+++ exercises/02-exejs_obj_long.js
@@ -0,0 +1,30 @@
+const user = {
+    name: "Luis Ricardo",
+    id:8
+};
+
+const book1 = {
+    title: 'Mossau',
+    author: 'Somebody',
+    isbn: '0316769487',
+    category: 'Triller'
+    };
+
+const book2 = {
+    title: 'Mustafa',
+    author: 'jean pier',
+    isbn: '0446310786',
+    category: 'Triller'
+    };
+
+user.books = [book1, book2]
+
+console.log( user)
+
+const library = [];
+
+
+
+
+
+console.log(user)
